f67e66b80101f5c3000234c0c374f52e8d0be5dc
Lisa P <eisteemango@gmail.com>
2013-04-09 17:46:46 +0200

    Rearange index.html

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
+// Main JS
 
+$(document).ready(function() {
+
+/** 
+*   Implement steallar.js for parallax,
+*   but not on mobile devices and small screens, cause it can cause trouble.
+*   Check for touch (-> mobile) w/ modernizr and screen width and then apply stellar
+*/
+    if(! Modernizr.touch && $(window).width() > 800) {
+        $(window).stellar();
+    }
+
+});
