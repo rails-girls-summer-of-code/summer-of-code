4fc59fc7e7211219472c6e3781e7ecd10a9ea095
Lisa P <eisteemango@gmail.com>
2013-04-16 02:14:44 +0200

    Add contact, projects, edit text

--- js/main.js
+++ js/main.js
@@ -7,25 +7,52 @@ $(document).ready(function() {
 *   but not on mobile devices and small screens, cause it can cause trouble.
 *   Check for touch (-> mobile) w/ modernizr and screen width and then apply stellar
 */
-    if(! Modernizr.touch && $(window).width() > 800) {
-        $(window).stellar();
-    }
+	if(! Modernizr.touch && $(window).width() > 800) {
+			$(window).stellar();
+	}
+/**
+ * Simple scroll effects on the front page
+ */
+	$('.scroll').click(function(e) {
+			var aim = $(this).attr('href');
+			e.preventDefault();
+			
+			$('html,body').animate({
+					scrollTop: $(aim).offset().top
+			}, 1500);
+	})
+/**
+ * Accordion on the faq
+ */
+	// $('.accordion').accordion({
+	// 	heightStyle: "content",
+	// 	icons: {
+	// 		"header": "accordion-header",
+	// 		"activeHeader": "accordion-header-active"
+	// 	}
+	// });
 
-    $('.scroll').click(function(e) {
-        var self = $(this);
-        e.preventDefault();
-        var ziel = self.parent().find('.wrapper');
+/**
+ * Show/ hide navbar
+ */
+	$(function() {
 
-        console.log(ziel.offset());
-        // $('html,body').animate({
-        //     scrollTop: ziel.offsetTop()
-        // }, 2000);
-    })
-    $('.accordion').accordion({
-        heightStyle: "content",
-        icons: {
-          "header": "accordion-header",
-          "activeHeader": "accordion-header-active"
-        }
-    });
+		var header = $('.intro h2');
+		var height = header.height();
+		var pos = Math.floor(header.position().top);
+		var scroll = height + pos;
+		
+		$(window).scroll(function() {
+			var offset = $(window).scrollTop();
+			if($(window).width() > 780) {
+				if(offset > scroll) {
+					$('.header-container').removeClass('no-scroll');
+				} else {
+					$('.header-container').addClass('no-scroll');
+				}
+			}
+		})
+	})
+
+	
 });
