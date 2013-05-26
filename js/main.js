4eb36dae6cd2870bb6bd91b4e5df59550b1e1d05
Sven Fuchs <me@svenfuchs.com>
2013-05-26 19:51:47 +0200

    add a campaign page, use proper permalink style

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function() {
 
-/** 
+/**
 *   Implement steallar.js for parallax,
 *   but not on mobile devices and small screens, cause it can cause trouble.
 *   Check for touch (-> mobile) w/ modernizr and screen width and then apply stellar
@@ -16,7 +16,7 @@ $(document).ready(function() {
 	$('.scroll').click(function(e) {
 			var aim = $(this).attr('href');
 			e.preventDefault();
-			
+
 			$('html,body').animate({
 					scrollTop: $(aim).offset().top - 80
 			}, 1500);
@@ -42,7 +42,7 @@ $(document).ready(function() {
 		var height = header.height();
 		var pos = Math.floor(header.position().top);
 		var scroll = height + pos;
-		
+
 		$(window).scroll(function() {
 			var offset = $(window).scrollTop();
 			if($(window).width() > 780) {
@@ -55,5 +55,41 @@ $(document).ready(function() {
 		})
 	})
 
-	
+  $('.donate-button').click(function(){
+    var self = $(this);
+    var amount = 0;
+    if (self.attr('data-custom')) {
+      amount = $('#custom-donation-amount').val();
+      if (amount === "") {
+        amount = 75;
+      }
+    } else {
+      amount = self.attr('data-amount');
+    }
+    amount = amount * 100;
+
+    var package = self.attr('data-name');
+
+    var token = function(res) {
+      $('input[name="stripe_card_token"]').val(res.id);
+      $('input[name="amount"]').val(amount);
+      $('input[name="package"]').val(package);
+      $('form#stripe-form').submit();
+    };
+    var description = package == 'Custom' ? 'Your donation' : package + ' Sponsorship';
+
+    StripeCheckout.open({
+      key:         'pk_test_1z7JS6h82UpmSL1gzmrRKxAa',
+      address:     false,
+      amount:      amount,
+      currency:    'usd',
+      name:        'Rails Girls Summer of Code',
+      description: description,
+      panelLabel:  'Donate',
+      token:       token
+    });
+
+    return false;
+  });
+
 });
