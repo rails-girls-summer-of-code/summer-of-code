245efec710cb051efcda9e5fa1ea4b2b95060c98
Lisa P <passinglisa@gmail.com>
2013-07-24 22:13:08 +0200

    completely redo index.html

--- js/main.js
+++ js/main.js
@@ -3,27 +3,6 @@
 $(document).ready(function() {
 
 /**
-*   Implement steallar.js for parallax,
-*   but not on mobile devices and small screens, cause it can cause trouble.
-*   Check for touch (-> mobile) w/ modernizr and screen width and then apply stellar
-*/
-	if(! Modernizr.touch && $(window).width() > 800) {
-			$(window).stellar();
-	}
-/**
- * Simple scroll effects on the front page
- */
-	$('.scroll').click(function(e) {
-			var aim = $(this).attr('href');
-      var step = parseInt($(this).attr('data-step'));
-			e.preventDefault();
-
-      console.log(Math.pow(6, step))
-			$('html,body').animate({
-					scrollTop: $(aim).offset().top - 120 + Math.pow(8, step)
-			}, 1500);
-	})
-/**
  * Accordion on the faq
  */
 	$('.accordion').accordion({
@@ -34,29 +13,7 @@ $(document).ready(function() {
 		}
 	});
 
-/**
- * Show/ hide navbar
- */
-	$(function() {
-
-		var header = $('.intro h2');
-    if (header.length == 0) return;
-		var height = header.height();
-		var pos = Math.floor(header.position().top);
-		var scroll = height + pos;
-
-		$(window).scroll(function() {
-			var offset = $(window).scrollTop();
-			if($(window).width() > 780) {
-				if(offset > scroll) {
-					$('.header-container').removeClass('no-scroll');
-				} else {
-					$('.header-container').addClass('no-scroll');
-				}
-			}
-		})
-	});
-
+/* Donation Campaign */
   $(function() {
     if(location.hostname == 'localhost') {
       $('#stripe-form').attr('action', 'http://localhost:3000/donations/checkout');
