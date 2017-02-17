f3cf8177e5438f715f09af6f207954b79f579ad2
Maria Ronacher <mkalinina.it@gmail.com>
2017-02-17 11:11:36 +0100

    Fixed urls from http to https

--- assets/javascripts/stats.js
+++ assets/javascripts/stats.js
@@ -28,7 +28,7 @@ Stats.Data = function(callback) {
 };
 $.extend(Stats.Data, {
   URLS: {
-    production:  'http://campaign.railsgirlssummerofcode.org/donations/stats.json',
+    production:  'https://campaign.railsgirlssummerofcode.org/donations/stats.json',
     development: 'http://localhost:3000/donations/stats.json'
   },
 });
